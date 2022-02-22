d9b31ba7e703787fa32ad8121e3398bf54631259
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-02-22 15:42:49 +0100

    CreateFormComponent

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "../src/css/AppLogin.css";
 import Login from "./components/Authontication/Login/Login";
 import Register from "./components/Authontication/Register/Register";
+import AddUser from "./components/User/AddUser";
+
 function App() {
   return (
-    <div className="app">
-        <Login />
-        <div className="auth"> 
-            <Register />
-        </div>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route exact path="/" component={Login}></Route>
+          <Route path="/register" component={Register}></Route>
+          <Route path="/admin/owner/create" component={AddUser}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
