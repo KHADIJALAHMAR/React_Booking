18b3e46fb3dcac4a03930abccf68834aed2b7ca7
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-02-25 16:19:13 +0100

    createPanelCustomer

--- src/components/Admin_Dashboard/Panels/Customers/List.js
+++ src/components/Admin_Dashboard/Panels/Customers/List.js
@@ -4,16 +4,32 @@ import Item from "./Item";
 const List = () => {
   return (
     <div>
-      <table class="table align-items-center mb-0">
-          <thead>
-            <tr>
-              <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Name</th>
-              <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Email</th>
-              <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Gender</th>
-            </tr>
-          </thead>
-        <Item />
-      </table>
+      <div className="card-body px-0 pb-2">
+        <div className="table-responsive">
+          <table class="table align-items-center mb-0">
+            <thead>
+              <tr>
+                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                  Companies
+                </th>
+                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                  Members
+                </th>
+                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                  Budget
+                </th>
+                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                  Completion
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <Item />
+              <Item />
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Admin_Dashboard/Panels/Panel.js
+++ src/components/Admin_Dashboard/Panels/Panel.js
@@ -1,10 +1,20 @@
 import React from "react";
 import List from "./Customers/List";
+import Header from "./Customers/Header";
 
 const Panel = () => {
   return (
     <div>
-      <List />
+      <div className="container-fluid py-4">
+        <div className="row my-4">
+          <div className="col-lg-8 col-md-6 mb-md-0 mb-4">
+            <div class="card">
+              <Header />
+              <List />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
