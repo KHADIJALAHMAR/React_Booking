c01769e5bca9ec557e96d20a1291cc9d99bd673f
khadija <khadijalahmar8@gmail.com>
2022-02-25 16:33:04 +0100

    ✅ add type file in form add hotel

--- src/components/Admin_Dashboard/Crud/Hotel/AddHotel.js
+++ src/components/Admin_Dashboard/Crud/Hotel/AddHotel.js
@@ -10,7 +10,7 @@ const AddHotel= () => {
         images :"",
         stars : ""
     });
-    
+
     const [submitted, setSubmitted] = useState(false);
 
     const handleName = (e) => {
@@ -19,9 +19,9 @@ const AddHotel= () => {
     const handleDescreption = (e) => {
         setAddHotel({ ...AddHotel, descreption: e.target.value });
     };
-    const handleImage_cover = (e) => {
-        setAddHotel({ ...AddHotel, image_cover: e.target.value });
-    };
+    const handleFileSelect = (e) => {
+      setAddHotel({...AddHotel , image_cover: e.target.files[0]})
+    }
     const handleImages= (e) => {
         setAddHotel({ ...AddHotel, images: e.target.value });
     };
@@ -31,7 +31,6 @@ const AddHotel= () => {
 
 
     const handleSubmit = (e) => {
-     
         e.preventDefault();
         if (
         AddHotel.name &&
@@ -53,71 +52,44 @@ const AddHotel= () => {
         }
         setSubmitted(true);
       };
-
-
     return (
-        
-        <form
-        className="auth_register_form"
-        method="POST"
-        action=""
-        onSubmit={handleSubmit}
-        >
-        <div className="auth_form_element">
-          <input
-            value={AddHotel.name}
-            onChange={handleName}
-            type="text"
-            name="username"
-            id="username"
-            placeholder="NameHotel"
-          />
-        </div>
-        <div className="auth_form_element">
-          <input
-            value={AddHotel.descreption}
-            onChange={handleDescreption}
-            type="text"
-            name="text"
-            id="text"
-            placeholder="descreption Hotel "
-          />
-        </div>
-        <div className="auth_form_element">
-          <input
-            value={AddHotel.image_cover}
-            onChange={handleImage_cover}
-            type="text"
-            name="image_cover"
-            id="image_cover"
-            placeholder="image_cover"
-          />
-        </div>
-        <div className="auth_form_element">
-          <input
-            value={AddHotel.images}
-            onChange={handleImages}
-            type="text"
-            name="image"
-            id="repeat_password"
-            placeholder="image"
-          />
-        </div>
-        
-        <div className="auth_form_element">
-          <select name="star" id="star" onChange={handleStars}>
-            <option selected disabled>
-              Choose a Stars
-            </option>
-            <option value="star1">1</option>
-            <option value="star2">2</option>
-            <option value="star3">3</option>
-            <option value="star4">4</option>
-          </select>
-        </div>
-        <button type="submit">Create  Hotel</button>
-        </form>
-        
+        <div>
+              <form className="auth_register_form" method="POST" action="" onSubmit={handleSubmit}>
+
+                  <div className="mb-3">
+                    <input type="text" className="form-control" placeholder="Name" aria-label="Name" aria-describedby="email-addon" value={AddHotel.name }  onChange={handleName}/>
+                    </div>
+
+                  <div className="mb-3">
+                    <input type="text" className="form-control" placeholder="Descreption" aria-label="Email" aria-describedby="email-addon" alue={AddHotel.descreption } onChange={handleDescreption} />
+                  </div>
+
+                  <div className="mb-3">
+                    <input type="file" className="form-control" placeholder="image_cover" aria-label="Password" aria-describedby="password-addon"  value={AddHotel.image_cover} onChange={handleFileSelect} />
+                  </div>
+
+                  <div className="mb-3">
+                    <input type="text" className="form-control" placeholder="image_cover" aria-label="Email" aria-describedby="password-addon"  value={AddHotel.images} onChange={handleImages} />
+                  </div>  
+
+                  <div className="mb-3">
+                  <select  className="form-control"  aria-label="Email"   aria-describedby="email-addon" name="star" id="star" onChange={handleStars} placeholder="Star">
+                              <option selected disabled>
+                                Choose a Stars
+                              </option>
+                              <option value="star1">1</option>
+                              <option value="star2">2</option>
+                              <option value="star3">3</option>
+                              <option value="star4">4</option>
+                            </select>
+                  </div>
+
+                  <div className="text-center">
+                    <button type="button" className="btn bg-gradient-dark w-100 my-4 mb-2">Sign up</button>
+                  </div>
+              </form>
+
+        </div>  
     )
     }
 
