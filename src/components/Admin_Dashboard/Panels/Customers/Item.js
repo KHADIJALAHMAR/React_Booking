99e5b52b297a6196ffc453925d542a1f85ee6a26
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-03-01 13:56:45 +0100

    deleteFileGetCustomersFromServicesFolder

--- src/components/Admin_Dashboard/Panels/Customers/Item.js
+++ src/components/Admin_Dashboard/Panels/Customers/Item.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import getCustomer from "../../../../services/GetCustomer";
 
 const Item = () => {
   const [customers, setCustomers] = useState([]);
