79dfa81f5a3aa0968fda934444917990f17f17ab
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-03-07 18:28:38 +0100

    addListsInPanelComponent

--- src/components/Admin_Dashboard/Panels/Customers/List.js
+++ src/components/Admin_Dashboard/Panels/Customers/List.js
@@ -1,10 +1,11 @@
-
 import React from "react";
 import Item from "./Item";
+import Header from "./Header";
 
 const List = () => {
   return (
     <div>
+      <Header />
       <div className="card-body px-0 pb-2">
         <div className="table-responsive">
           <table class="table align-items-center mb-0">
@@ -13,9 +14,7 @@ const List = () => {
                 <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
                   Customer name
                 </th>
-                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
-                  
-                </th>
+                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2"></th>
                 <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
                   Customer email
                 </th>
