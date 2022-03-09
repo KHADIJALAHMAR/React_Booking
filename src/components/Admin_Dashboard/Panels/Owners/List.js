73a1f455130002504abb44d16d20477bba8f91a3
Mohammed Kabli <76818640+mohammedkabli@users.noreply.github.com>
2022-03-09 16:41:16 +0100

    ✅ list owner

--- src/components/Admin_Dashboard/Panels/Owners/List.js
+++ src/components/Admin_Dashboard/Panels/Owners/List.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import Item from "./Item";
+import Header from "./Header";
 
-function List() {
+const List = () => {
   return (
-    <div>List owners</div>
-  )
-}
+    <div>
+      <Header />
+      <div className="card-body px-0 pb-2">
+        <div className="table-responsive">
+          <table className="table align-items-center mb-0">
+            <thead>
+              <tr>
+                <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
+                  Owner name
+                </th>
+                <th className="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                  Owner email
+                </th>
+                <th className="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                  Gender
+                </th>
+                <th className="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                  Role
+                </th>
+                <th className="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                  Actions
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <Item />
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default List
+export default List;
