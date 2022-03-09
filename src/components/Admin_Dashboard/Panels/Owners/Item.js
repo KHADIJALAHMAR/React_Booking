998d8c4b89378540fbc7d7394068f60140e36364
Mohammed Kabli <76818640+mohammedkabli@users.noreply.github.com>
2022-03-09 16:40:46 +0100

    ✅ add items component for owner

--- src/components/Admin_Dashboard/Panels/Owners/Item.js
+++ src/components/Admin_Dashboard/Panels/Owners/Item.js
@@ -1,9 +1,105 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function Item() {
+const Item = () => {
+  const [owners, setOwners] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/admin/owners/`)
+      .then((res) => {
+        setOwners(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
   return (
-    <div>Item</div>
-  )
-}
+    <>
+      {owners.map((owner, index) => (
+        <tr key={index}>
+          <td className="align-middle text-center text-sm">
+            {/* <h6 class="mb-0 text-sm"></h6> */}
+            <span className="text-xs font-weight-bold">
+              {owner.username}
+            </span>
+          </td>
+          {/* <td> */}
+          {/* <div class="avatar-group mt-2"> */}
+          {/* <a
+              href="javascript:;"
+              class="avatar avatar-xs rounded-circle"
+              data-bs-toggle="tooltip"
+              data-bs-placement="bottom"
+              title=""
+              data-bs-original-title="Ryan Tompson"
+            >
+              <img src="../assets/img/team-1.jpg" alt="team1" />
+            </a>
+            <a
+              href="javascript:;"
+              class="avatar avatar-xs rounded-circle"
+              data-bs-toggle="tooltip"
+              data-bs-placement="bottom"
+              title=""
+              data-bs-original-title="Romina Hadid"
+            >
+              <img src="../assets/img/team-2.jpg" alt="team2" />
+            </a>
+            <a
+              href="javascript:;"
+              class="avatar avatar-xs rounded-circle"
+              data-bs-toggle="tooltip"
+              data-bs-placement="bottom"
+              title=""
+              data-bs-original-title="Alexander Smith"
+            >
+              <img src="../assets/img/team-3.jpg" alt="team3" />
+            </a>
+            <a
+              href="javascript:;"
+              class="avatar avatar-xs rounded-circle"
+              data-bs-toggle="tooltip"
+              data-bs-placement="bottom"
+              title=""
+              data-bs-original-title="Jessica Doe"
+            >
+              <img src="../assets/img/team-4.jpg" alt="team4" />
+            </a> */}
+          {/* </div> */}
+          {/* </td> */}
+          <td className="align-middle text-center text-sm">
+            <span className="text-xs font-weight-bold">{owner.email}</span>
+          </td>
+          <td className="align-middle text-center">
+            <span className="text-xs font-weight-bold">
+              {owner.role.name}
+            </span>
+            {/* <div class="progress-wrapper w-75 mx-auto">
+            <div class="progress-info">
+              <div class="progress-percentage">
+                <span class="text-xs font-weight-bold"></span>
+              </div>
+            </div>
+            <div class="progress">
+              <div
+                class="progress-bar bg-gradient-info w-60"
+                role="progressbar"
+                aria-valuenow="60"
+                aria-valuemin="0"
+                aria-valuemax="100"
+              ></div>
+            </div>
+          </div> */}
+          </td>
+          <td className="align-middle text-center">
+          <a class="btn btn-link text-danger text-gradient px-3 mb-0" href="javascript:;"><i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete</a>
+          <a class="btn btn-link text-dark px-3 mb-0" href="javascript:;"><i class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Edit</a>
+          </td>
+        </tr>
+      ))}
+    </>
+  );
+};
 
-export default Item
+export default Item;
