d5a74fe29335e9d0f8b9a7ce4be310cd93683ed9
khadija <khadijalahmar8@gmail.com>
2022-03-10 16:26:35 +0100

    ⏪ change in file Room service

--- src/services/RoomService.js
+++ src/services/RoomService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const APP_URL = "http://localhost:4000/rooms"; 
 
-export async function deleteHotel(id) {
+export async function deleteRoom(id) {
     await axios.delete(`${APP_URL}/room/:roomId`, {
         id
     });
