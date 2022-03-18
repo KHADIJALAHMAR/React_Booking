a1a344d43a08bfedacb33b5ba01a0745ae7ce801
Mohammed Kabli <76818640+mohammedkabli@users.noreply.github.com>
2022-03-18 09:55:20 +0100

    🐛 fix the path

--- src/components/Admin_Dashboard/Panels/Hotels/Item.js
+++ src/components/Admin_Dashboard/Panels/Hotels/Item.js
@@ -61,7 +61,7 @@ function Item ({handleDeleteObject}){
           href="javascript:;"><i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete</a>
           <Link class="btn btn-link text-dark px-3 mb-0"
           onClick={() => Update(hotel.id) }
-           to="/dashboard/update">
+           to={"/dashboardowner/hotel/update/" + hotel._id}>
            <i class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Edit</Link>
           </td>
           
