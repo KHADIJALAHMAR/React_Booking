1ebf41588c405aa8443cd4b96cd08657356a25e1
Mohammed Kabli <76818640+mohammedkabli@users.noreply.github.com>
2022-03-21 12:20:08 +0100

    🚧 finished get owners for admin

--- src/components/Admin_Dashboard/Panels/Owners/Item.js
+++ src/components/Admin_Dashboard/Panels/Owners/Item.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
+import {getOwners} from "../../../../services/UserService"
 
-function Item (){
+
+function Item (handleDeleteObject){
     const [data , setData] = useState([]);
 
     useEffect(() => {
@@ -10,56 +12,111 @@ function Item (){
     },[]);
 
     const getOwners = async () => {
-    const  res =  await axios.get("http://localhost:4000/admin/owners")
-      if(res.status === 200) {
-        setData(res.data);
-      }
+      await axios.get("http://localhost:4000/admin/owners")
+      .then(result => {
+         setData(result.data.owners)
+        //  console.log(result.data.owners);
+        })
+      .catch(error => {
+         console.error(error);
+           throw error;
+         });
     };
-    console.log("data :",data);
- 
-  return (
-    <>
-       { data && data.map((items, index) => {
-        return (
-          <tr key={index}>
+    console.log(data);
+
+  //  const testData = data.owners
+      return (
+        <>
+       {data.map((owner, index) => (
+        <tr key={index}>
           <td className="align-middle text-center text-sm">
             <span className="text-xs font-weight-bold">
-              {items.username}
+              {owner.username}
             </span>
           </td>
           <td className="align-middle text-center text-sm">
-            <span className="text-xs font-weight-bold">{items.email}</span>
+            <span className="text-xs font-weight-bold">{owner.email}</span>
           </td>
           <td className="align-middle text-center">
             <span className="text-xs font-weight-bold">
-              {items.role.name}
+              {owner.role.name}
             </span>
           </td>
           <td className="align-middle text-center">
             <a
               class="btn btn-link text-danger text-gradient px-3 mb-0"
-              // onClick={(e) => {
-              //   e.preventDefault();
-              //   handleDeleteObject("user", items._id);
-              // }}
+              onClick={(e) => {
+                e.preventDefault();
+                handleDeleteObject("user", owner._id);
+              }}
               href="javascript:;"
             >
               <i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete
             </a>
-            <a class="btn btn-link text-dark px-3 mb-0" href="javascript:;">
-              <i
-                class="fas fa-pencil-alt text-dark me-2"
-                aria-hidden="true"
-              ></i>
-              Edit
-            </a>
-            <Link class="btn btn-link text-dark px-3 mb-0" to={"/dashboard/user/update/" + items._id}><i class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Edit</Link>
+            
+            <Link class="btn btn-link text-dark px-3 mb-0" to={"/dashboard/user/update/" + owner._id}><i class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Edit</Link>
           </td>
         </tr>
-        )
-        })} 
-    </>
-  );
+      ))}
+        </>
+      );
+    
+ 
+ 
 };
 
 export default Item;
+
+
+
+// function Item (){
+//   const [owners, setOwners] = useState([]);
+
+//   useEffect(() =>{
+//     (async ()=> {
+//       await getOwners().then(owners => {
+//         setOwners(owners)
+//       })
+//     })()
+//   },[]);
+
+//   if(owners.status === 200){
+//     return (
+//       <div>
+//         {owners.map((owner, index)=>{
+//           <tr key={index}>
+//             <td className="align-middle text-center text-sm">
+//               <span className="text-xs font-weight-bold">
+//                 {owner.username}
+//               </span>
+//             </td>
+//             <td className="align-middle text-center text-sm">
+//               <span className="text-xs font-weight-bold">{owner.email}</span>
+//             </td>
+//             <td className="align-middle text-center">
+//               <span className="text-xs font-weight-bold">
+//                 {owner.role.name}
+//               </span>
+//             </td>
+//             <td className="align-middle text-center">
+//               <a
+//                 class="btn btn-link text-danger text-gradient px-3 mb-0"
+//                 // onClick={(e) => {
+//                 //   e.preventDefault();
+//                 //   handleDeleteObject("user", owner._id);
+//                 // }}
+//                 href="javascript:;"
+//               >
+//                 <i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete
+//               </a>
+              
+//               <Link class="btn btn-link text-dark px-3 mb-0" to={"/dashboard/user/update/" + owner._id}><i class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Edit</Link>
+//             </td>
+//           </tr>
+//         })}
+//       </div>
+//     );
+//   };
+ 
+// }
+// export default Item;
