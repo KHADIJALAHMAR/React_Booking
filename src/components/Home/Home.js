40094dda8f42bc8f3d3955a6f6ae9c5736d4151d
UssefBhl <ybhl83@gmail.com>
2022-03-24 10:31:43 +0100

    💄 finished the selects style

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useSelector , useDispatch} from 'react-redux'
-import { Link } from 'react-router-dom'
 import Body from './Body'
 import NavBar from '../Shared_Elements/NavBar'
 import HeroHeader from './HeroHeader/HeroHeader'
